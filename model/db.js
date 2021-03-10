a5ce37f98f5f582eb676b0d915d04a6f00c30d00
Rajasegar Chandran <rajasegar.c@gmail.com>
2021-03-10 06:08:33 +0530

    chore: add mongodburi from env

--- model/db.js
+++ model/db.js
@@ -3,82 +3,80 @@ var mongoose = require('mongoose');
 var creationInfo = require('./creationInfo');
 var modifiedOn = require('./modifiedOn');
 // Build the connection string
-var dbURI = "mongodb://localhost/MongoosePM";
+var dbURI = process.env.MONGODB_URI || "mongodb://localhost/MongoosePM";
 
 // Create the database connection
 mongoose.connect(dbURI);
 
 mongoose.connection.on('connected',function(){
-	
-	console.log('Mongoose connected to ' + dbURI);
+
+  console.log('Mongoose connected to ' + dbURI);
 });
 
 mongoose.connection.on('error',function(err){
-	console.log('Mongoose connection error: ' + err);
+  console.log('Mongoose connection error: ' + err);
 });
 
 mongoose.connection.on('disconnected',function(){
-	console.log('Mongoose disconnected');
+  console.log('Mongoose disconnected');
 });
 
 process.on('SIGINT',function(){
-	mongoose.connection.close(function(){
-		console.log('Mongoose disconnected through app termination');
-		process.exit(0);
-		
-	});
+  mongoose.connection.close(function(){
+    console.log('Mongoose disconnected through app termination');
+    process.exit(0);
+
+  });
 });
 
 /****************************************************
-	USER SCHEMA
-	***********************************************/
+        USER SCHEMA
+ ***********************************************/
 var userSchema = new mongoose.Schema({
-	name:String,
-	email:{type:String,unique:true},
-	createdOn:{type:Date,default:Date.now},
-	lastLogin:Date
+  name:String,
+  email:{type:String,unique:true},
+  createdOn:{type:Date,default:Date.now},
+  lastLogin:Date
 });
 
 userSchema.plugin(modifiedOn);
-
-// Build the User model
-mongoose.model('User',userSchema);
+// Build the User model mongoose.model('User',userSchema);
 
 var lengthValidator = function(val){
-	if(val && val.length >= 5){
-		return true;
-	}
-	return false;
+  if(val && val.length >= 5){
+    return true;
+  }
+  return false;
 };
 var validateLength = [lengthValidator,'Too short'];
 /************************************************
-	TASK SCHEMA
-************************************************/
+        TASK SCHEMA
+ ************************************************/
 var taskSchema = new mongoose.Schema({
-	taskName:{type:String,required:true,validate:validateLength},
-	taskDesc:String,
-	createdOn:{type:Date,default:Date.now},
-	createdBy:{type:mongoose.Schema.Types.ObjectId,ref:'User',required:true},
-	assignedTo:{type:mongoose.Schema.Types.ObjectId,ref:'User'}
+  taskName:{type:String,required:true,validate:validateLength},
+  taskDesc:String,
+  createdOn:{type:Date,default:Date.now},
+  createdBy:{type:mongoose.Schema.Types.ObjectId,ref:'User',required:true},
+  assignedTo:{type:mongoose.Schema.Types.ObjectId,ref:'User'}
 });
 
 taskSchema.plugin(modifiedOn);
 /****************************************************
-	PROJECT SCHEMA
-****************************************************/
+        PROJECT SCHEMA
+ ****************************************************/
 var projectSchema  = new mongoose.Schema({
-	projectName:{type:String,unique:true,required:true},
-	createdBy:{type:mongoose.Schema.Types.ObjectId,ref:'User'},
-	contributors:[{type:mongoose.Schema.Types.ObjectId,ref:'User'}],
-	tasks:[taskSchema]
-	
+  projectName:{type:String,unique:true,required:true},
+  createdBy:{type:mongoose.Schema.Types.ObjectId,ref:'User'},
+  contributors:[{type:mongoose.Schema.Types.ObjectId,ref:'User'}],
+  tasks:[taskSchema]
+
 });
 
 projectSchema.plugin(creationInfo);
 projectSchema.plugin(modifiedOn);
 
 projectSchema.statics.findByUserID = function(userid,callback){
-	this.find({createdBy:userid},'_id projectName',{sort:'modifiedOn'},callback);
+  this.find({createdBy:userid},'_id projectName',{sort:'modifiedOn'},callback);
 };
 
 
